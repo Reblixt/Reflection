4094f634b725863dee62714e7435fd16bcd462cd
Reblixt <carl@klofverskjold.se>
2026-02-05 09:37:26 +0100

    Added the angular built in components

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from './hero/hero';
 import { Setup } from './setup/setup';
+import { Projects } from './projects/projects';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, Setup],
+  imports: [RouterOutlet, Hero, Setup, Projects],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
